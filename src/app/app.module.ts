e8d74d4eb3fe3e9ea1ac05c32820d52d82ecc27b
DzakySalman <dzakysalman4@gmail.com>
2023-10-28 09:51:45 +0700

    A_UTS212102402

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { FooterComponent } from './footer/footer.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    EcommerceComponent,
+    FooterComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule
